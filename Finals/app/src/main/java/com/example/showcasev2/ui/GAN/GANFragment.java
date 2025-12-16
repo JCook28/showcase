a5abdc2262c3c1e5d42174c3c7e8e20924db0780
JCook28 <johana.cook@stu.evsck12.com>
2025-12-16 13:48:54 -0600

    added GAN

---
+++ Finals/app/src/main/java/com/example/showcasev2/ui/GAN/GANFragment.java
@@ -0,0 +1,38 @@
+package com.example.showcasev2.ui.GAN;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.showcasev2.R;
+
+public class GANFragment extends Fragment {
+
+    private GANViewModel mViewModel;
+
+    public static GANFragment newInstance() {
+        return new GANFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_g_a_n, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(GANViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ Finals/app/src/main/java/com/example/showcasev2/ui/GAN/GANViewModel.java
@@ -0,0 +1,7 @@
+package com.example.showcasev2.ui.GAN;
+
+import androidx.lifecycle.ViewModel;
+
+public class GANViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
